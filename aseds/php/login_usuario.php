904eb854925f5157ca27084973831e706f77a8c2
santiagomon07 <148169516+santiagomon07@users.noreply.github.com>
2023-10-30 15:41:44 -0500

    Encriptacion de contraseña en registro y login

--- aseds/php/login_usuario.php
+++ aseds/php/login_usuario.php
@@ -1,23 +1,21 @@
 <?php
 session_start();
-include 'conexion.php';
+include'conexion.php';
 
 $correo = $_POST['correo'];
 $contrasena = $_POST['contrasena'];
+$contrasena = hash('sha512', $contrasena);
 
-//$validar_login = mysqli_query($conexion, "SELECT * FROM usuarios WHERE correo = '$correo' and contrasena = '$contrasena'");
-$row = mysqli_query($conexion, "SELECT * FROM usuarios WHERE correo = '$correo'");
 
-//$row = $validar_login->fetch_row();
-$validar_login=$row->fetch_array(MYSQLI_ASSOC);
-$hash='$2y$10$p4tvCvYyxsEUck3R8am5ruL2JmHOYQH1oovzrK/ud8m';
-if(password_verify($contrasena, $hash)){
-  // $_SESSION['usuario'] = $correo;
+$validar_login = mysqli_query($conexion, "SELECT * FROM usuarios WHERE
+ correo = '$correo' and contrasena = '$contrasena'");
+
+if(mysqli_num_rows($validar_login) > 0){
+    $_SESSION['usuario'] = $correo;
     header("location: ../bienvenido.php");
     exit;
 }else{
-
-    echo '
+    echo'
     <script>
        alert("Usuario o contraseña incorrecta, verifique los datos ingresados");
        window.location = "../index.php";
--- aseds/php/registro.php
+++ aseds/php/registro.php
@@ -5,9 +5,9 @@ include'conexion.php';
 $nombre_completo = $_POST['nombre_completo'];
 $correo = $_POST['correo'];
 $usuario = $_POST['usuario'];
-//$contrasena = $_POST['contrasena'];
+$contrasena = $_POST['contrasena'];
 //incriptamiento de contraseña
-$contrasena = password_hash($_POST['contrasena'], PASSWORD_DEFAULT);
+$contrasena = hash('sha512', $contrasena);
 
 
 $query = "INSERT INTO usuarios(nombre_completo, correo, usuario, contrasena)
